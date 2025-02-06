8dfa68c65cb5022e4402f3da4c11cf8082f48bb3
VictorPedroza <victor242206@gmail.com>
2025-02-06 08:18:37 -0300

    First Commit

---
+++ back-end/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Server is Running"
+    })
+})
+
+app.listen(8088, () => {
+    console.log("Typescript server is Running!")
+})
